34381815a3b5c98268f4ea56f5535e52ee29ed2e
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-23 09:11:40 -0400

    JSONP respnse on server

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var fs = require("fs");
+var querystring = require("querystring");
 
 var wizard = fs.readFileSync("wizard.html").toString();
 
@@ -7,7 +8,13 @@ http.createServer(function(req, res) {
 	if(req.url == "/") {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(wizard);
-	} else {
+	} else if(req.url.slice(0, "/fetch/".length) == "/fetch/") {
+		var data = JSON.parse(querystring.unescape(req.url.slice("/fetch/".length)));
 
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end("JSONPCallback("+JSON.stringify(data)+")");
+	} else {
+		console.log("Unknown endpoint "+req.url);
+		console.log(req.url.slice(0, "/fetch/".length));
 	}
-}).listen(8080);
+}).listen(8888);
