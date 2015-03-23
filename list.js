1eba33ec985c06e1635d53e63930a93220f95f3a
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-23 09:16:05 -0400

    Better server architecture

--- list.js
+++ list.js
@@ -4,6 +4,9 @@ var querystring = require("querystring");
 
 var wizard = fs.readFileSync("wizard.html").toString();
 
+var wizardCounter = 0;
+var wizards = {};
+
 http.createServer(function(req, res) {
 	if(req.url == "/") {
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -11,8 +14,31 @@ http.createServer(function(req, res) {
 	} else if(req.url.slice(0, "/fetch/".length) == "/fetch/") {
 		var data = JSON.parse(querystring.unescape(req.url.slice("/fetch/".length)));
 
+		var response = null;
+
+		if(data.type == "auth") {
+			wizards[wizardCounter] = [];
+
+			// TODO: prevent collisions, maybe
+			response = {
+				type: "auth",
+				token: wizardCounter++
+			};
+		} else if(data.type == "add") {
+			if(wizards[data.token]) {
+				wizards[data.token].push(data.product);
+			}
+
+			response = {
+				type: "add",
+				success: true
+			}
+		} else if(data.type == "remove") {
+			console.logo("TODO: remove");
+		}
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end("JSONPCallback("+JSON.stringify(data)+")");
+		res.end("JSONPCallback("+JSON.stringify(response)+")");
 	} else {
 		console.log("Unknown endpoint "+req.url);
 		console.log(req.url.slice(0, "/fetch/".length));
